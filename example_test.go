dc8c0dd74fb38adb505e229f97b1160d0d5a04aa
txthinking <cloud@txthinking.com>
2024-10-08 20:53:26 +0800

    call stop at most once

--- example_test.go
+++ example_test.go
@@ -2,6 +2,7 @@ package runnergroup_test
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net/http"
 	"time"
@@ -12,33 +13,52 @@ import (
 func Example() {
 	g := runnergroup.New()
 
-	s := &http.Server{
+	s1 := &http.Server{
 		Addr: ":9991",
 	}
 	g.Add(&runnergroup.Runner{
 		Start: func() error {
-			return s.ListenAndServe()
-		},
-		Stop: func() error {
-			return s.Shutdown(context.Background())
-		},
-	})
-
-	s1 := &http.Server{
-		Addr: ":9992",
-	}
-	g.Add(&runnergroup.Runner{
-		Start: func() error {
+			log.Println("start1")
 			return s1.ListenAndServe()
 		},
 		Stop: func() error {
+			log.Println("stop1")
 			return s1.Shutdown(context.Background())
 		},
 	})
 
+	s2 := &http.Server{
+		Addr: ":9992",
+	}
+	g.Add(&runnergroup.Runner{
+		Start: func() error {
+			log.Println("start2")
+			return errors.New("fail")
+			// return s2.ListenAndServe()
+		},
+		Stop: func() error {
+			log.Println("stop2")
+			return s2.Shutdown(context.Background())
+		},
+	})
+
+	s3 := &http.Server{
+		Addr: ":9993",
+	}
+	g.Add(&runnergroup.Runner{
+		Start: func() error {
+			log.Println("start3")
+			return s3.ListenAndServe()
+		},
+		Stop: func() error {
+			log.Println("stop3")
+			return s3.Shutdown(context.Background())
+		},
+	})
+
 	go func() {
-		time.Sleep(5 * time.Second)
-		log.Println(g.Done())
+		time.Sleep(3 * time.Second)
+		g.Done()
 	}()
 	log.Println(g.Wait())
 	// Output:
--- runnergroup.go
+++ runnergroup.go
@@ -1,6 +1,8 @@
 package runnergroup
 
 import (
+	"encoding/json"
+	"errors"
 	"sync"
 	"time"
 )
@@ -8,24 +10,26 @@ import (
 // RunnerGroup is like sync.WaitGroup,
 // the diffrence is if one task stops, all will be stopped.
 type RunnerGroup struct {
-	Runners []*Runner
-	End     chan byte
+	Runners       []*Runner
+	once_stop     func()
+	start_results map[int]error
+	stop_results  map[int]error
+	mutex         *sync.Mutex
 }
 
 type Runner struct {
 	// Start is a blocking function.
 	Start func() error
 	// Stop is not a blocking function, if Stop called, must let Start return.
-	// Notice: Stop maybe called multi times even if before start.
-	Stop   func() error
-	lock   sync.Mutex
-	status int
+	Stop func() error
 }
 
 func New() *RunnerGroup {
 	g := &RunnerGroup{}
 	g.Runners = make([]*Runner, 0)
-	g.End = make(chan byte)
+	g.start_results = map[int]error{}
+	g.stop_results = map[int]error{}
+	g.mutex = &sync.Mutex{}
 	return g
 }
 
@@ -33,64 +37,94 @@ func (g *RunnerGroup) Add(r *Runner) {
 	g.Runners = append(g.Runners, r)
 }
 
-// Call Wait after all task have been added,
-// Return the first ended start's result.
+// Call Wait after all tasks have been added,
 func (g *RunnerGroup) Wait() error {
-	e := make(chan error)
-	for _, v := range g.Runners {
-		v.status = 1
-		go func(v *Runner) {
+	g.once_stop = sync.OnceFunc(func() {
+		time.Sleep(3 * time.Second)
+		var wg sync.WaitGroup
+		for i, v := range g.Runners {
+			wg.Add(1)
+			go func(i int, v *Runner) {
+				defer wg.Done()
+				g.mutex.Lock()
+				if _, ok := g.start_results[i]; ok {
+					g.stop_results[i] = errors.New("_")
+					g.mutex.Unlock()
+					return
+				}
+				g.mutex.Unlock()
+				err := v.Stop()
+				g.mutex.Lock()
+				g.stop_results[i] = err
+				g.mutex.Unlock()
+			}(i, v)
+		}
+		wg.Wait()
+	})
+
+	var wg sync.WaitGroup
+	for i, v := range g.Runners {
+		wg.Add(1)
+		go func(i int, v *Runner) {
+			defer wg.Done()
 			err := v.Start()
-			v.lock.Lock()
-			v.status = 0
-			v.lock.Unlock()
-			select {
-			case <-g.End:
-			case e <- err:
-			}
-		}(v)
+			g.mutex.Lock()
+			g.start_results[i] = err
+			g.mutex.Unlock()
+			g.once_stop()
+		}(i, v)
 	}
-	err := <-e
-	for _, v := range g.Runners {
-		for {
-			v.lock.Lock()
-			if v.status == 0 {
-				v.lock.Unlock()
-				break
-			}
-			v.lock.Unlock()
-			_ = v.Stop()
-			time.Sleep(300 * time.Millisecond)
+	wg.Wait()
+
+	g.mutex.Lock()
+	defer g.mutex.Unlock()
+	e := &Error{
+		Start: make([]string, len(g.Runners)),
+		Stop:  make([]string, len(g.Runners)),
+	}
+	ok := true
+	for i, v := range g.start_results {
+		if v == nil {
+			e.Start[i] = ""
+		}
+		if v != nil {
+			e.Start[i] = v.Error()
+		}
+		if ok && v != nil {
+			ok = false
 		}
 	}
-	close(g.End)
-	return err
-}
-
-// Call Done if you want to stop all.
-// return the stop's return which is not nil, do not guarantee,
-// because starts may ended caused by itself.
-func (g *RunnerGroup) Done() error {
-	if len(g.Runners) == 0 {
+	for i, v := range g.stop_results {
+		if v == nil {
+			e.Stop[i] = ""
+		}
+		if v != nil {
+			e.Stop[i] = v.Error()
+		}
+		if ok && v != nil {
+			ok = false
+		}
+	}
+	if ok {
 		return nil
 	}
-	var e error
-	for _, v := range g.Runners {
-		for {
-			v.lock.Lock()
-			if v.status == 0 {
-				v.lock.Unlock()
-				break
-			}
-			v.lock.Unlock()
-			if err := v.Stop(); err != nil {
-				if e == nil {
-					e = err
-				}
-			}
-			time.Sleep(300 * time.Millisecond)
-		}
-	}
-	<-g.End
 	return e
+}
+
+// Call Done to stop all tasks.
+func (g *RunnerGroup) Done() {
+	g.once_stop()
+}
+
+type Error struct {
+	Start []string
+	Stop  []string
+}
+
+func (e *Error) Error() string {
+	b, err := json.Marshal(e)
+	if err != nil {
+		return err.Error()
+	}
+	return string(b)
 }
